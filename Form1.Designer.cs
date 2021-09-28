5eeb9395c1ea4bed1ba10fd442d55cc3184e6251
McCarthybob3 <mccarthybob3@gmail.com>
2021-09-27 22:04:50 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace test_image_editor_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(624, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(349, 81);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(95, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(596, 271);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(794, 106);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Chad";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(794, 193);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Virgin";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 378);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(456, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(185, 335);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "image";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 466);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_image_editor_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked)
+            {
+                this.pictureBox1.Image = CreateBitmapAtRuntime2();
+                this.pictureBox1.Visible = true;
+            }
+            else
+            {
+                this.pictureBox1.Image = CreateBitmapAtRuntime();
+                this.pictureBox1.Visible = true;
+            }
+        }
+
+        private Bitmap CreateBitmapAtRuntime()
+        {
+
+            Bitmap img;
+            string input = textBox1.Text;
+            Bitmap newImage;
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (radioButton1.Checked == true)
+            {
+                img = Properties.Resources.chad;
+            }
+            else
+            {
+                img = Properties.Resources.wojak;
+            }
+
+            Bitmap bmp = img;
+            newImage = bmp;
+            Font bigFont = new Font(SystemFonts.DefaultFont.FontFamily, 7, FontStyle.Regular);
+
+            Graphics gr = Graphics.FromImage(newImage);
+            gr.DrawImageUnscaled(bmp, 0, 0);
+            gr.DrawString(input, bigFont, Brushes.Black,
+            new RectangleF(400, ((bmp.Height / 2) - 50), 300, 300));
+            string path = Path.Combine(desktop, "newimg.jpg");
+            newImage.Save(path);
+            return newImage;
+            
+        }
+
+
+        private Bitmap CreateBitmapAtRuntime2()
+        {
+
+            Bitmap img;
+            string input = textBox1.Text;
+            Bitmap newImage;
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (radioButton1.Checked == true)
+            {
+                img = Properties.Resources.chad;
+            }
+            else
+            {
+                img = Properties.Resources.wojak;
+            }
+
+
+
+            DialogResult result = openFileDialog1.ShowDialog();
+     
+            string img2;
+
+            img2 = openFileDialog1.FileName;
+
+            Bitmap bmp = img;
+            newImage = bmp;
+
+            Bitmap bmp2 = (Bitmap)Bitmap.FromFile(img2);
+            Bitmap bmp3 = new Bitmap(bmp2.Width, bmp2.Height);
+
+
+
+            Graphics gr = Graphics.FromImage(newImage);
+            gr.DrawImage(bmp2, 400, ((bmp.Height / 2) - 50), bmp2.Width / 4, bmp2.Width / 4);
+            string path = Path.Combine(desktop, "newimg.jpg");
+            newImage.Save(path);
+            return newImage;
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
